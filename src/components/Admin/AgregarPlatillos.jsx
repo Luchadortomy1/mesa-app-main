a4c04455f5afc4ddd5a3f8ad7b1a978217bf90d8
Roberto Beltran Garcia <elrobert741@gmail.com>
2025-04-02 14:39:38 -0700

    Update AgregarPlatillos.jsx
    Prueba?

--- src/components/Admin/AgregarPlatillos.jsx
+++ src/components/Admin/AgregarPlatillos.jsx
@@ -1,3 +1,15 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import './AgregarPlatillos.css';
 
+const PanelAgregarPlatillos = ({ usuario }) => {
+
+
+    return (
+        <div>
+            <h1>Hola</h1>
+        </div>
+    );
+};
+    
+export default PanelAgregarPlatillos;
